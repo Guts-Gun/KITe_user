68ffa5951c96c11961950c3eeb9b19d8621190aa
L-Jiyong <leejiyong96@naver.com>
2023-01-10 20:51:28 +0900

    init setting ver0.31

--- src/main/java/gutsandgun/kite_user/config/ReadDBConfig.java
+++ src/main/java/gutsandgun/kite_user/config/ReadDBConfig.java
@@ -1,6 +1,9 @@
 package gutsandgun.kite_user.config;
 
+import com.sun.tools.jconsole.JConsoleContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +15,11 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @PropertySource({ "classpath:application.yml" })
@@ -26,6 +31,11 @@ import java.util.HashMap;
 public class ReadDBConfig {
     @Autowired
     private Environment env;
+
+    @Autowired
+    private JpaProperties jpaProperties;
+    @Autowired
+    private HibernateProperties hibernateProperties;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean ReadEntityManager() {
@@ -39,13 +49,18 @@ public class ReadDBConfig {
         em.setJpaVendorAdapter(vendorAdapter);
 
         //Hibernate 설정
-        HashMap<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto",env.getProperty("spring.jpa.hibernate.ddl-auto"));
-        properties.put("hibernate.dialect",env.getProperty("spring.jpa.properties.hibernate.dialect"));
-        properties.put("hibernate.dialect.storage_engine",env.getProperty("spring.jpa.properties.hibernate.storage_engine"));
-        properties.put("hibernate.format_sql",env.getProperty("spring.jpa.properties.hibernate.format_sql"));
-        properties.put("hibernate.show-sql",env.getProperty("spring.jpa.properties.hibernate.show-sql"));
-        properties.put("hibernate.generate-ddl",env.getProperty("spring.jpa.properties.hibernate.generate-ddl"));
+        Map<String, Object> properties = hibernateProperties.determineHibernateProperties(jpaProperties.getProperties(), new HibernateSettings());
+        //Hibernate 설정
+//        HashMap<String, Object> properties = new HashMap<>();
+//        properties.put("hibernate.hbm2ddl.auto",env.getProperty("spring.jpa.hibernate.ddl-auto"));
+//        properties.put("hibernate.dialect",env.getProperty("spring.jpa.properties.hibernate.dialect"));
+//        properties.put("hibernate.dialect.storage_engine",env.getProperty("spring.jpa.properties.hibernate.storage_engine"));
+//        properties.put("hibernate.format_sql",env.getProperty("spring.jpa.properties.hibernate.format_sql"));
+//        properties.put("hibernate.show-sql",env.getProperty("spring.jpa.properties.hibernate.show-sql"));
+//        properties.put("hibernate.generate-ddl",env.getProperty("spring.jpa.properties.hibernate.generate-ddl"));
+//        properties.put("hibernate.naming.physical-strategy", env.getProperty("spring.jpa.hibernate.naming.physical-strategy"));
+//        properties.put("hibernate.naming.implicit_naming_strategy", env.getProperty("spring.jpa.hibernate.naming.implicit-strategy"));
+        System.out.println(properties);
         em.setJpaPropertyMap(properties);
         return em;
     }
