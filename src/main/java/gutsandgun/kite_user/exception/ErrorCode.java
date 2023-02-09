a621636aa5124709c85dda956e0e4e4cf9536831
Seb <sbscsue@gmail.com>
2023-02-09 11:25:55 +0900

    [FIX]ErrorCode

--- src/main/java/gutsandgun/kite_user/exception/ErrorCode.java
+++ src/main/java/gutsandgun/kite_user/exception/ErrorCode.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    BAD_REQUEST(400, "S000", "잘못된 요청입니다.");
-
+    BAD_REQUEST(400, "S000", "잘못된 요청입니다."),
+    UN_AUTHORIZATION(401, "S001", "토큰값이 없습니다");
 
     private int status;
     private String code;
